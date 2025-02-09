0f92a227effe583b620c6efaac3481f386a4df03
Tristan Graaff <tristangraaff@yahoo.com>
2025-02-09 20:53:46 +0100

    implemented getVisitorCount function

--- visitorCounter.js
+++ visitorCounter.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     async function incrementVisitorCount() {
         try {
-            let response = await fetch('https://y0218osvh0.execute-api.eu-central-1.amazonaws.com/prod/', 
+            const response = await fetch('https://y0218osvh0.execute-api.eu-central-1.amazonaws.com/prod/', 
                 { method: 'POST' });
             if (!response.ok) throw new Error('Network response was not ok');
             console.log('Visitor count incremented');
@@ -12,16 +12,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     async function getVisitorCount() {
         try {
-            let response = await fetch('https://y0218osvh0.execute-api.eu-central-1.amazonaws.com/prod/',
+            const response = await fetch('https://y0218osvh0.execute-api.eu-central-1.amazonaws.com/prod/',
                 { method: 'GET' });
             if (!response.ok) throw new Error('Network response was not ok');
-            let data = await response.json();
-            document.getElementById('visitor-count').innerText = data.visitor_count;
+            const data = await response.json();
+            const responseBody = JSON.parse(data.body)
+            console.log("Visitor count: " + responseBody.visitorCount);
+            document.getElementById('visitor-count').innerText = responseBody.visitorCount;
         } catch (error) {
             console.error('There has been a problem with your fetch operation:', error);
         }
     }
 
     incrementVisitorCount();
-    //getVisitorCount();
+    getVisitorCount();
 });
